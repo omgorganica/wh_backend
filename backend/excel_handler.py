d1d4b7b57374383fd32b2c2790e7ccfac8cff98c
omgorganica@yandex.ru <Rmztv9if>
2020-12-12 14:36:07 +0500

    create_or_update

--- backend/excel_handler.py
+++ backend/excel_handler.py
@@ -1,29 +1,40 @@
-from pprint import pprint
-
+import logging
 import pandas as pd
 from datetime import timedelta
 
 
 def handle_excel(path):
-    df = pd.read_excel(path)
-    df = df[['Work Date', 'Time From', 'Pers No', 'Overall Transports', 'Boxes Picked Picking',
-             'Loadings']].sort_values(by=['Work Date', 'Time From'], ascending=[True, True]).reset_index(drop=True)
+	df = pd.read_excel(path)
+	essential_headers = ['Work Date', 'Time From', 'Pers No', 'Overall Transports', 'Boxes Picked Picking',
+						 'Loadings']
 
-    def work_shift(row):
-        dd = str(row['Time From'])
-        h, m, s = dd.split(':')
-        h = int(h)
+	file_headers = list(df)
+	try:
+		for header in file_headers:
+			if header not in essential_headers:
+				raise ValueError('File do not contains essential headers ')
+	except ValueError as err:
+		logging.warning(err)
+		return
 
-        if h in range(20, 24):
-            return row['Work Date'] + timedelta(1)
-        else:
-            return row['Work Date']
+	df = df[['Work Date', 'Time From', 'Pers No', 'Overall Transports', 'Boxes Picked Picking',
+			 'Loadings']].sort_values(by=['Work Date', 'Time From'], ascending=[True, True]).reset_index(drop=True)
 
-    df['DATE'] = df.apply(work_shift, axis=1)
-    final = df.groupby(['Pers No', 'DATE'], sort=True).sum().reset_index()
-    final_dict = final.to_dict(orient='records')
-    return final_dict
+	def work_shift(row):
+		dd = str(row['Time From'])
+		h, m, s = dd.split(':')
+		h = int(h)
+
+		if h in range(20, 24):
+			return row['Work Date'] + timedelta(1)
+		else:
+			return row['Work Date']
+
+	df['DATE'] = df.apply(work_shift, axis=1)
+	final = df.groupby(['Pers No', 'DATE'], sort=True).sum().reset_index()
+	final_dict = final.to_dict(orient='records')
+	return final_dict
 
 
 if __name__ == '__main__':
- pass
+	pass
--- backend/views.py
+++ backend/views.py
@@ -1,6 +1,6 @@
 from django.db.models import F
 from .serializers import UserSerializer, ShiftResultSerializer, GoodSerializer, OrderSerializer, \
-    BalanceModifierSerializer, BalanceModifierHistorySerializer, FileUploaderSerializer
+	BalanceModifierSerializer, BalanceModifierHistorySerializer, FileUploaderSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import viewsets, status, permissions
 from rest_framework.response import Response
@@ -8,91 +8,95 @@ from .models import User, ShiftResult, Good, Order, BalanceModifier, BalanceModi
 from .excel_handler import handle_excel
 from django.conf import settings
 import os
+import logging
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
+	serializer_class = UserSerializer
+	queryset = User.objects.all()
 
 
 class ShiftViewSet(viewsets.ModelViewSet):
-    serializer_class = ShiftResultSerializer
-    queryset = ShiftResult.objects.all()
+	serializer_class = ShiftResultSerializer
+	queryset = ShiftResult.objects.all()
 
 
 class GoodsViewSet(viewsets.ModelViewSet):
-    serializer_class = GoodSerializer
-    queryset = Good.objects.all()
+	serializer_class = GoodSerializer
+	queryset = Good.objects.all()
 
 
 class OrderViewSet(viewsets.ModelViewSet):
-    serializer_class = OrderSerializer
-    queryset = Order.objects.all()
+	serializer_class = OrderSerializer
+	queryset = Order.objects.all()
 
 
 class FileUploaderViewSet(viewsets.ModelViewSet):
-    serializer_class = FileUploaderSerializer
-    queryset = FileUploader.objects.all()
+	serializer_class = FileUploaderSerializer
+	queryset = FileUploader.objects.all()
 
-    def create(self, request):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            filename = os.path.split(serializer.data['file'])[1]
-            results = handle_excel(os.path.join(settings.BASE_DIR, 'media\\files',
-                                                filename))  # Список словарей, содержащих итоги смены каждого сотрудника
-            balance_modifier = BalanceModifier.objects.get(
-                for_shift_result=True)  # Единственный модификатор, использующийся автоматически для итогов смены
-            superuser = User.objects.filter(
-                is_superuser=True).first()  # Не принципиально какой superuser будет числится в assigned_by У BalanceModifierHistory
-            for item in results:
-                try:
-                    picking = item['Boxes Picked Picking'] / settings.WORK_GOAL['picking']
-                    shifting = item['Overall Transports'] / settings.WORK_GOAL['shifting']
-                    loading = item['Loadings'] / settings.WORK_GOAL['loadings']
-                    result = round((picking + shifting + loading), 1)
-                    user = User.objects.get(wms_id=item['Pers No'])
+	def create(self, request):
+		serializer = self.get_serializer(data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			filename = os.path.split(serializer.data['file'])[1]
+			try:
+				results = handle_excel(os.path.join(settings.BASE_DIR, 'media\\files',
+													filename))  # Список словарей, содержащих итоги смены каждого сотрудника
+			except Exception as e:
+				logging.error(e)
+			balance_modifier = BalanceModifier.objects.get(
+				for_shift_result=True)  # Единственный модификатор, использующийся автоматически для итогов смены
+			superuser = User.objects.filter(
+				is_superuser=True).first()  # Не принципиально какой superuser будет числится в assigned_by У BalanceModifierHistory
+			for item in results:
+				try:
+					picking = item['Boxes Picked Picking'] / settings.WORK_GOAL['picking']
+					shifting = item['Overall Transports'] / settings.WORK_GOAL['shifting']
+					loading = item['Loadings'] / settings.WORK_GOAL['loadings']
+					result = round((picking + shifting + loading), 1)
+					user = User.objects.get(wms_id=item['Pers No'])
 
-                    '''
+					'''
                     Если суммарная производительность по 3 операциям сотрудника за смену больше 1,
                     то создается новый модификатор баланса, добавляющий ему на счет виртуальную валюту.
                     Модификатор увеличивается пропорционально результату смены
                     '''
-                    if result > 1:
-                        user.current_balance = F('current_balance') + balance_modifier.delta * result
-                        user.save(update_fields=['current_balance'])
+					if result > 1:
+						user.current_balance = F('current_balance') + balance_modifier.delta * result
+						user.save(update_fields=['current_balance'])
 
-                        BalanceModifierHistory.objects.create(
-                            assigned_by=superuser,
-                            assigned_to=user,
-                            modifier=balance_modifier,
-                            comment='Автоматическое пополнение по итогам смены'
-                        )
+						BalanceModifierHistory.objects.create(
+							assigned_by=superuser,
+							assigned_to=user,
+							modifier=balance_modifier,
+							comment='Автоматическое пополнение по итогам смены'
+						)
 
-                    '''
+					'''
                     Создание результата смены для каждого сотрудника. Если смена уже есть- обновить данные
                     '''
 
-                    ShiftResult.objects.update_or_create(date=item['DATE'], user=user, defaults={
-                        'picking': item['Boxes Picked Picking'],
-                        'transportations': item['Overall Transports'],
-                        'loading': item['Loadings'], 'result': result})
+					ShiftResult.objects.update_or_create(date=item['DATE'], user=user, defaults={
+						'picking': item['Boxes Picked Picking'],
+						'transportations': item['Overall Transports'],
+						'loading': item['Loadings'], 'result': result})
 
-                except Exception as e:
-                    print(e)
-            return Response(serializer.data)
+				except Exception as e:
+					print(e)
+			return Response(serializer.data)
 
 
 class BalanceModifierViewSet(viewsets.ModelViewSet):
-    serializer_class = BalanceModifierSerializer
-    queryset = BalanceModifier.objects.all()
+	serializer_class = BalanceModifierSerializer
+	queryset = BalanceModifier.objects.all()
 
 
 class BalanceModifierHistoryViewSet(viewsets.ModelViewSet):
-    serializer_class = BalanceModifierHistorySerializer
-    queryset = BalanceModifierHistory.objects.all()
+	serializer_class = BalanceModifierHistorySerializer
+	queryset = BalanceModifierHistory.objects.all()
 
-    def create(self, request):
-        serializer = FileUploaderSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
+	def create(self, request):
+		serializer = FileUploaderSerializer(data=request.data)
+		if serializer.is_valid():
+			serializer.save()
